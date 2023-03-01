1fc3dc560005fba3466c856f84e89671b8684c2c
buayanil <herrahmadfadhil@gmail.com>
2023-03-01 19:47:13 +0100

    commit

--- klaer-dir-berlin/src/App.js
+++ klaer-dir-berlin/src/App.js
@@ -17,12 +17,76 @@ function App() {
           <input type="submit" id="login-form-submit" className="button" />
         </form>
         <div className="footer loginFooter" id="loginFooter"></div>
+      </div>
+
+    <div className="small-div" id="MainScreen">
+      <p id="WilkommenNachricht">Wilkommen!</p>
+      <div id="Liste" className="box"></div>
+      <div>
+        <button id="AddButton">Add</button>
+        <button id="LogoutButton">Logout</button>
+      </div>
+      <div id="map" className="box"><Map /></div>
+      <div className="footerbar box" id="mainFooter"></div>
     </div>
 
-      <div className="small-div"><Map /></div>
-      <div className="small-div">Div 3</div>
-      <div className="small-div">Div 4</div>
+      <div className="small-div"><div id="AddScreen" class="row">
+  <form id="AddForm" class="box">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"/>
+    <br/>
+    <label for="ID">ID:</label>
+    <input type="text" id="ID" name="id"/>
+    <br/>
+    <label for="Adresse">Adresse:</label>
+    <input type="text" id="Adresse" name="Adresse"/>
+    <br/>
+    <label for="PLZ">PLZ:</label>
+    <input type="text" id="PLZ" name="Adresse"/>
+    <br/>
+    <label for="Stadt">Stadt:</label>
+    <input type="text" id="Stadt" name="Stadt"/>
+    <br/>
+    <label for="Lat">Lat:</label>
+    <input type="text" id="Lat" name="Lat"/>
+    <br/>
+    <label for="Long">Long:</label>
+    <input type="Long" id="Long" name="Long"/>
+    <br/>
+    <button id="Save">Save</button>
+  </form>
     </div>
+  </div>
+    
+  <div className="small-div" id="UpdateScreen">
+    <form id="UpdateForm" class="box">
+      <label for="name">Name:</label>
+      <input type="text" id="upname" name="name"/>
+      <br/>
+      <label for="ID">ID:</label>
+      <input type="text" id="upID" name="id"/>
+      <br/>
+      <label for="Adresse">Adresse:</label>
+      <input type="text" id="upAdresse" name="Adresse"/>
+      <br/>
+      <label for="PLZ">PLZ:</label>
+      <input type="text" id="upPLZ" name="Adresse"/>
+      <br/>
+      <label for="Stadt">Stadt:</label>
+      <input type="text" id="upStadt" name="Stadt"/>
+      <br/>
+      <label for="Lat">Lat:</label>
+      <input type="text" id="upLat" name="Lat"/>
+      <br/>
+      <label for="Long">Long:</label>
+      <input type="Long" id="upLong" name="Long"/>
+      <br/>
+      <button id="UpdateButton">Update</button>
+      <button id="DeleteButton">Delete</button>
+    </form>
+  </div>
+  
+</div>
   );
 }
 
--- klaer-dir-berlin/src/Map.js
+++ klaer-dir-berlin/src/Map.js
@@ -5,7 +5,7 @@ function Map() {
   const position = [51.505, -0.09];
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: "400px" }}>
+    <MapContainer center={position} zoom={13} style={{ height: "300px", width: "750px"}}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={position}>
         <Popup>
